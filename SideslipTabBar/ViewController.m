5f38d6c8c73be6438f1d0fc0290df737bd5e9edd
zhangqingfeng <jim@jason.local>
2013-07-09 13:47:37 +0800

    Initial Commit

---
+++ SideslipTabBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SideslipTabBar
+//
+//  Created by zhangqingfeng on 13-7-9.
+//  Copyright (c) 2013年 zhangqingfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
